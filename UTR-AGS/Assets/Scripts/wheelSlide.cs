7b1240899849715cf0945ce063431aa00095e9d8
ElRetonoVR <st4319@utr.edu.mx>
2025-07-31 15:05:33 -0600

    Rodillo Funcional
    Now, the rodillo can make the function rotate.

--- UTR-AGS/Assets/Scripts/wheelSlide.cs
+++ UTR-AGS/Assets/Scripts/wheelSlide.cs
@@ -7,18 +7,22 @@ public class wheelSlide : MonoBehaviour
     Rigidbody wheelRigidbody;
     Collider wheelCollider;
     Transform wheelTransform;
-    public float slideTop, slideBottom,threshold;
+    public float slideTop, slideBottom, threshold;
     public bool isSlide, isGrabbing;
-    public Vector3 test;
+    public GameObject test;
     public GameObject[] gameController;
+    Vector3 rayo1, rayo2;
+    [SerializeField] float wheelCurrentPosition = 0;
+    public float angle;
+    [SerializeField] int checkPoints = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        
+
     }
     void Start()
     {
-        test = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        test = GameObject.Find("wheelPosition");
         wheelRigidbody = GetComponent<Rigidbody>();
         wheelCollider = GetComponent<Collider>();
         wheelTransform = GetComponent<Transform>();
@@ -26,14 +30,80 @@ public class wheelSlide : MonoBehaviour
     }
     void FixedUpdate()
     {
-       
-        Debug.DrawRay(new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z+0.6f), gameController[0].transform.position, Color.blue);
+
+
     }
     // Update is called once per frame
     void Update()
     {
+        if (checkPoints == 6)
+        {
+            EndOfGame();
+        }
+        if (this.transform.rotation.eulerAngles.z >= 1 && this.transform.rotation.eulerAngles.z <= 59)
+        {
+            if (checkPoints == 0)
+            {
+                checkPoints++;
+            }
+            else if (checkPoints == 2)
+            {
+                checkPoints--;
+            }
+        }
+        else if (this.transform.rotation.eulerAngles.z >= 60 && this.transform.rotation.eulerAngles.z <= 119)
+        {
+            if (checkPoints == 1)
+            {
+                checkPoints++;
+            }
+            else if (checkPoints == 3)
+            {
+                checkPoints--;
+            }
+        }
+        else if (this.transform.rotation.eulerAngles.z >= 120 && this.transform.rotation.eulerAngles.z <= 179)
+        {
+            if (checkPoints == 2)
+            {
+                checkPoints++;
+            }
+            else if (checkPoints == 4)
+            {
+                checkPoints--;
+            }
+        }
+        else if (this.transform.rotation.eulerAngles.z >= 180 && this.transform.rotation.eulerAngles.z <= 239)
+        {
+            if (checkPoints == 3)
+            {
+                checkPoints++;
+            }
+            else if (checkPoints == 5)
+            {
+                checkPoints--;
+            }
+        }
+        else if (this.transform.rotation.eulerAngles.z >= 240 && this.transform.rotation.eulerAngles.z <= 299)
+        {
+            if (checkPoints == 4)
+            {
+                checkPoints++;
+            }
+            else if (checkPoints == 6)
+            {
+                checkPoints--;
+            }
+        }
+        else if (this.transform.rotation.eulerAngles.z >= 300 && this.transform.rotation.eulerAngles.z <= 359)
+        {
+            if (checkPoints == 5)
+            {
+                checkPoints++;
+            }
 
-
+        }
+       
         if (this.transform.position.y > slideTop)
         {
             this.transform.position = new Vector3(this.transform.position.x, slideTop, this.transform.position.z);
@@ -45,16 +115,18 @@ public class wheelSlide : MonoBehaviour
         if (isSlide)
         {
             this.transform.position = new Vector3(this.transform.position.x, slideBottom, this.transform.position.z);
-        }else if (!isGrabbing && !isSlide)
+        }
+        else if (!isGrabbing && !isSlide)
         {
             this.transform.position = new Vector3(this.transform.position.x, slideTop, this.transform.position.z);
         }
-       
+
 
 
     }
     bool oneEntered;
     Ray ray;
+
     void OnTriggerStay(Collider other)
     {
         if (!isSlide)
@@ -90,21 +162,29 @@ public class wheelSlide : MonoBehaviour
             {
                 if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
                 {
-                    ray = new Ray(new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z+0.6f), other.transform.position);
-                    Debug.DrawRay(ray.origin, ray.direction, Color.green,5f);
+
+                    ray = new Ray(test.transform.position, gameController[0].transform.position - test.transform.position);
+                    rayo1 = (ray.direction);
+                    Debug.DrawRay(ray.origin, ray.direction, Color.green, 5f);
                 }
                 if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
                 {
-                    //Physics.Raycast(ray);  
-                    
+                    Ray ray2 = new Ray(test.transform.position, (gameController[0].transform.position - test.transform.position));
+                    rayo2 = (ray2.direction);
+                    Physics.Raycast(ray2.origin, ray2.direction);
+                    Debug.DrawRay(test.transform.position, (gameController[0].transform.position - test.transform.position), Color.blue);
+                    angle = Vector3.SignedAngle(rayo1, rayo2, Vector3.back);
+
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (wheelCurrentPosition - angle)));
+
                 }
-                else
+                if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger))
                 {
-                    isGrabbing = false;
+                    wheelCurrentPosition = this.transform.rotation.eulerAngles.z;
                 }
 
             }
-            
+
         }
     }
     void OnTriggerExit(Collider other)
@@ -113,7 +193,19 @@ public class wheelSlide : MonoBehaviour
         {
             isGrabbing = false;
             oneEntered = false;
-         }
+        }
     }
 
+    void EndOfGame()
+    {
+         #if UNITY_EDITOR
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+#else 
+        Application.Quit();
+#endif
+    }
 }
